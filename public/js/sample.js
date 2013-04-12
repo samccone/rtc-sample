c0e2edf79f0fe29cf12f87c72528ae9188507f00
Kevin Ennis <kennis84@gmail.com>
2013-04-11 23:09:41 -0400

    just for fun

--- public/js/sample.js
+++ public/js/sample.js
@@ -1,69 +1,31 @@
 var ctx = new webkitAudioContext()
-  , pro = ctx.createScriptProcessor(512, 1, 1)
-  , buffers = []
   , src
 
 console.log = function(message) {
   document.getElementById('console').innerHTML += message + "\n";
 }
 
-var dataChannelChat = {
-  send: function(message) {
-    for(var connection in rtc.dataChannels) {
-      var channel = rtc.dataChannels[connection];
-      if ( channel.readyState != 'open' ) return;
-      channel.send(JSON.stringify(message));
-    }
-  },
-  recv: function(channel, message) {
-    return JSON.parse(message);
-  },
-  event: 'data stream data'
-};
-
-rtc.on(dataChannelChat.event, function() {
-  var d = dataChannelChat.recv.apply(this, arguments);
-  console.log(d)
-  if ( !d.length ) return;
-  buffers.push(new Float32Array(d));
-});
-
 rtc.on('add remote stream', function(stream, socketId) {
   console.log("ADDING REMOTE STREAM...");
-  rtc.attachStream(stream, socketId);
+  connect(stream);
 });
 
 rtc.on('connect', function() {
   console.log("** RTC connected");
-})
+});
 
-navigator.webkitGetUserMedia({audio: true}, connect, streamErr);
+rtc.createStream({audio: true}, function(){}, streamErr);
 
 function connect( stream ){
-  src = ctx.createMediaStreamSource(stream);
-  src.connect(pro);
-  pro.connect(ctx.destination);
-  pro.onaudioprocess = processor;
+  var audio = new Audio();
+  audio.src = webkitURL.createObjectURL(stream);
+  audio.play();
 }
 
 function streamErr(){
   alert('O noes! couldn\'t get ur inputz');
 }
 
-function processor(evt){
-  var input = evt.inputBuffer.getChannelData(0)
-    , output = evt.outputBuffer.getChannelData(0)
-    , buffer;
-  // send it
-  dataChannelChat.send(Array.prototype.slice.call(input, 0));
-  // play back buffers
-  if ( buffers.length )
-    output.set(buffers.shift());
-  else
-    output.set(new Float32Array(512))
-}
-
-
 window.onload = function() {
-  rtc.connect('ws://localhost:8001', '');
+  rtc.connect('ws://24.­250.­22.­134:­80001', '');
 }
